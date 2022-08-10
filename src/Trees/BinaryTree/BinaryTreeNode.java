ddfc56368d3ccf0b9711ffb5bc46b5e88802d5cb
Omkar Shinde <Omkar.Shinde@ril.com>
2022-08-10 19:04:30 +0530

    Added Binary tree use class

--- src/Trees/BinaryTree/BinaryTreeNode.java
+++ src/Trees/BinaryTree/BinaryTreeNode.java
@@ -4,4 +4,8 @@ public class BinaryTreeNode<T> {
     public T data;
     public BinaryTreeNode<T> left;
     public BinaryTreeNode<T> right;
+
+    public BinaryTreeNode(T data){
+        this.data = data;
+    }
 }
---
+++ src/Trees/BinaryTree/BinaryTreeUse.java
@@ -0,0 +1,11 @@
+package Trees.BinaryTree;
+
+public class BinaryTreeUse {
+    public static void main(String[] args) {
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(1);
+        BinaryTreeNode<Integer> node1 = new BinaryTreeNode<>(2);
+        root.left = node1;
+        BinaryTreeNode<Integer> node2 = new BinaryTreeNode<>(2);
+        root.right = node2;
+    }
+}
