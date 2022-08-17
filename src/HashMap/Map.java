6f5283d16b3354688080f23cecf29b2ca466193c
Omkar Shinde <Omkar.Shinde@ril.com>
2022-08-17 12:01:55 +0530

    Hashmap

--- src/HashMap/Map.java
+++ src/HashMap/Map.java
@@ -8,9 +8,9 @@ public class Map<K,V> {
     int numBuckets;
 
     public Map() {
-        numBuckets = 20;
+        numBuckets = 5;
         bucket = new ArrayList<>();
-        for(int i=0; i<20; i++){
+        for(int i=0; i<numBuckets; i++){
             bucket.add(null);
         }
     }
@@ -19,6 +19,30 @@ public class Map<K,V> {
     private int getBucketIndex(K key){
         int hashCode = key.hashCode();//hashCode() method is already defined in Object class
         return hashCode % numBuckets;
+    }
+
+    public double loadFactor(){
+        return (1.0*size)/numBuckets;
+    }
+    //Rehashing function
+    private void rehash(){
+        System.out.println("Rehashing buckets : " + numBuckets + " size " + size);
+        ArrayList<MapNode<K,V>> temp = bucket;
+        bucket = new ArrayList<>();
+        for(int i=0; i<2*numBuckets; i++){
+            bucket.add(null);
+        }
+        size = 0;
+        numBuckets *= 2;
+        for(int i=0; i< temp.size(); i++){
+            MapNode<K,V> head = temp.get(i);
+            while(head != null){
+                K key = head.key;
+                V value = head.value;
+                insert(key,value);
+                head = head.next;
+            }
+        }
     }
     //Insertion
     public void insert(K key, V value){
@@ -33,8 +57,13 @@ public class Map<K,V> {
         }
         head = bucket.get(bucketIndex);
         MapNode<K,V> newNode = new MapNode<>(key,value);
+        size++;
         newNode.next = head;
         bucket.set(bucketIndex,newNode);
+        double loadFactor = (1.0*size)/numBuckets;
+        if(loadFactor > 0.7){
+            rehash();
+        }
     }
     //getValue
     public V getValue(K key){
@@ -55,6 +84,7 @@ public class Map<K,V> {
         MapNode<K,V> previousNode = null;
         while(head != null){
             if(head.key.equals(key)){
+                size--;
                 if(previousNode == null){
                     bucket.set(bucketIndex,head.next);
                 }else {
