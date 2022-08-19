c7aaf8cea5305fb2380d424a0ad0db7e1842a2b5
Omkar Shinde <Omkar.Shinde@ril.com>
2022-08-19 10:38:07 +0530

    Implementing Min Heap

--- src/Heap/PriorityQueue.java
+++ src/Heap/PriorityQueue.java
@@ -24,6 +24,56 @@ public class PriorityQueue {
         }
         return heap.get(0);
     }
+    //Time Complexity = O(log n)
+    public static void insert(int ele){
+        heap.add(ele);
+        int childIndex = heap.size()-1;
+        int parentIndex = (childIndex-1)/2;
+        //Up-Heapify
+        while(childIndex > 0){
+            if(heap.get(childIndex) < heap.get(parentIndex)){
+                int temp = heap.get(childIndex);
+                heap.set(childIndex, heap.get(parentIndex));
+                heap.set(parentIndex,temp);
+                childIndex = parentIndex;
+                parentIndex = (childIndex-1)/2;
+            }else{
+                return;
+            }
+        }
 
+    }
+    //Time Complexity = O(log n)
+    public static int removeMin() throws PriorityQueueException {
+        if(isEmpty()){
+            throw new PriorityQueueException();
+        }
+        int temp = heap.get(0);
+        heap.set(0, heap.get(heap.size() - 1));
+        heap.remove(heap.size() - 1);
+        int parentindex = 0;
+        int minIndex = parentindex;
+        int leftChildIndex = 1;
+        int rightChildIndex = 2;
+        while(leftChildIndex < heap.size()){
+            if(heap.get(leftChildIndex) < heap.get(minIndex)){
+                minIndex = leftChildIndex;
+            }
+            if(rightChildIndex < heap.size() && heap.get(rightChildIndex) < heap.get(minIndex)){
+                minIndex = rightChildIndex;
+            }
+            if(minIndex == parentindex){
+                break;
+            }else{
+                int temp1 = heap.get(parentindex);
+                heap.set(parentindex, heap.get(minIndex));
+                heap.set(minIndex, temp1);
+                parentindex = minIndex;
+                leftChildIndex = 2 * parentindex + 1;
+                rightChildIndex = 2 * parentindex + 2;
+            }
+        }
+        return temp;
+    }
 
 }
